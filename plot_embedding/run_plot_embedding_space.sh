5ae4fa9173ab091eca3f9c97c46a8e10a0ca28d4
Yixuan Huang <hyx597822176@gmail.com>
2025-11-18 12:43:45 +0000

    update sh script to launch job to slurm

--- plot_embedding/run_plot_embedding_space.sh
+++ plot_embedding/run_plot_embedding_space.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-PREDICT_JOB=/share/nas2_3/yhuang/myrepo/mphys_project/plot_embedding_space.sh
+PREDICT_JOB=/share/nas2_3/yhuang/myrepo/mphys_project/plot_embedding/plot_embedding_space.sh
 sbatch --job-name=plot_embedding $PREDICT_JOB
