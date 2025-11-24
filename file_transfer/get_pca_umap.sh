8f3e76f7ad21e31952cde7002e5620515950c007
Yixuan Huang <hyx597822176@gmail.com>
2025-11-24 14:19:44 +0000

    create shell scripts to submit get_pca_umap.py to slurm

---
+++ file_transfer/get_pca_umap.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# ======== SLURM Job Config ========
+#SBATCH --time=0-04:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=16
+#SBATCH --constraint=A100
+#SBATCH --exclude=compute-0-7
+
+# ======== Path Config ========
+JOB=/share/nas2_3/yhuang/myrepo/mphys_project/file_transfer/get_pca_umap.py
+
+echo ">>> Job started at $(date)"
+
+# ======== Environment Setup ========
+source /share/nas2_3/yhuang/byol/runscripts/venv/bin/activate
+
+# ======== Run ========
+echo ">>> Running $JOB"
+python "$JOB"
+
+echo ">>> Job finished at $(date)"
